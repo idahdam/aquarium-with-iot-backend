66476281e24d48772e7b587fc0aa4169ff26c077
pademss <fatma.putri@ui.ac.id>
2022-10-19 16:39:41 +0700

    add config

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'mF/}<a5Q6)?j,2./g7!f=,"He?E>#.'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'dvUZr1axGm0DPuHOtfw/lA=='),
   },
 });
